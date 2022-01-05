9deba09109091ecadb50646d3c022b492a3399e9
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2022-01-05 20:55:04 -0300

    Bariloche - Resis
    Todo lo que hice en el viaje desde Bariloche hasta resistencia

--- Pruebas-de-html/app.js
+++ Pruebas-de-html/app.js
@@ -1,14 +1,14 @@
-const express = require ('Express');
+const express = require ('express');
 const app = express();
 const path = require ('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen (3040, () => {
-    console.log('El servidor funciona correctamente');
+app.listen (5000, () => {
+    console.log('El servidor funciona correctamente en el servidor 3040');
 });
 
 app.get ('/', (req, res) => {
-    res.sendFile (path.resolve (__dirname, './views/links.html'));
+    res.sendFile (path.resolve (__dirname, './views/mercadoliebre.html'));
 });
