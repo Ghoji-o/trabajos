10f7a0d82721475d512a3960eda211ebbdfcf0da
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2022-02-22 20:45:34 -0300

    Actualización de ML 22/02/2022

--- proyecto-en-playground/app.js
+++ proyecto-en-playground/app.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const path = require ('path');
-
 const app = express();
+
+
+app.set('view Engine', 'ejs');
 
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicPath))
@@ -10,9 +12,17 @@ app.get ('/', (req, res) => {
     res.sendFile (path.resolve(__dirname, './views/home.html'))
 })
 
+app.get ('/registro', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './views/registro.html'))
+});
+
+app.get ('/login', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './views/login.html'));
+});
+
 app.listen(4000, () => {
     console.log('El servidor funca en el puerto 4000');
-})
+});
 
 //npm i -D nodemon para instalar nodemon
 //npx nodemon app.js   para ejecutar
