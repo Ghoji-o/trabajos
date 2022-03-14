8497150ec9238ad86d6cc75929c0428a94f180ac
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2022-03-14 17:46:03 -0300

    Revert "Revert "actualización""
    This reverts commit 4b131ebf09f40c9e35b516d7049018bd6fc28bb3.

---
+++ mercadoLiebre/app.js
@@ -0,0 +1,8 @@
+const express = require ("express");
+const app = express;
+
+app.listen(3000, () => console.log('El servidor esta en el puerto 3k'));
+
+app.get('/', (req, res) => {
+    res.send('Hola')
+})
