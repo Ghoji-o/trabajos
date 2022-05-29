337e9f40c35381d9f92f5042167576b02cf1ecbe
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2022-05-29 00:39:15 -0300

    .

--- Mercado-Liebre/app.js
+++ Mercado-Liebre/app.js
@@ -26,26 +26,3 @@ app.listen(process.env.PORT|| 3000, () => {
 
 //npm i -D nodemon para instalar nodemon
 //npx nodemon app.js   para ejecutar
-
-/*Para el heroku */
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-let prefix = process.env.PREFIX;
-
-client.on('ready', () => {
-    client.user.setActivity('bot en heroku', {type: 'WATCHING'});
-    console.log('Listo!');
-
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+"ping")) {
-        let ping = Math.floor(message.client.ws.ping);
-        message.channel.send(':ping_pong: `'+ping+' ms.` desde heroku.'); 
-
-    }
-
-});
-
-client.login(process.env.TOKEN); 
